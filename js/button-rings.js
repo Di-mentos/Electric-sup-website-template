1b87085bf492e5a540d95fa9242441a221ff237f
Dmitry Zaparenko <102208063+M-Dimonka@users.noreply.github.com>
2022-04-21 10:45:05 +0300

    Update button-rings.js

--- js/button-rings.js
+++ js/button-rings.js
@@ -15,29 +15,10 @@ function scaling1(){
 			c1.style.transform = "scale(1)";
 			c1.style.opacity = "1";
 
-			//console.log("scale k = " + k);
 			k++;
 		}
 	})
 	setInterval(scaling1, 1500);
 }
 
-/*function scaling2(){
-	c2.style.transform = "scale(1.5)";
-	c2.style.opacity = "0";
-	c2.style.transition = "scale, 1s linear, opacity 0.8s linear 0.5s";
-	c2.addEventListener('transitionend', function f2(){
-		if(window.getComputedStyle(c2).opacity == "0"){
-			c2.style.transition = "";
-			c2.style.transform = "scale(1)";
-			c2.style.opacity = "1";
-			
-			//console.log("scale k2 = " + k2);
-			k2++;
-		}
-	})
-	setInterval(scaling2, 1500);
-}*/
-
 scaling1();
-//setTimeout(scaling2, 1500);
